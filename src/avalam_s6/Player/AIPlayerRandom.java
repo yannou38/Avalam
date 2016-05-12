8b77161bde3938cc2044a15dafc9e97adb732f37
Seawolf <Seawolf@R2D2>
2016-05-12 16:00:01 +0200

    Continued AIPlayerRandom

--- src/avalam_s6/Player/AIPlayerRandom.java
+++ src/avalam_s6/Player/AIPlayerRandom.java
@@ -5,9 +5,10 @@
  */
 package avalam_s6.Player;
 
-import avalam_s6.Core.Coordinate;
-import avalam_s6.Core.Grid;
+import avalam_s6.Core.*;
+
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  *
@@ -22,8 +23,39 @@ public class AIPlayerRandom extends AIPlayer{
     @Override
     public Coordinate[] play() {
         ArrayList<Coordinate[]> mesCoups = new ArrayList<>();
-        //TODO : finir
-        return null;
+        for (int i = 0; i < grid.getWidth(); i++)
+        {
+            /**
+             *    1 2 3
+             *    4 0 5
+             *    6 7 8
+             */   
+            for (int j = 0; j < grid.getHeight(); j++)
+            {
+                Coordinate c0 = new Coordinate(j,i);
+                if (c0.isValid() && grid.getCellAt(c0).getState() == State.TOWER)
+                {
+                    Coordinate c1 = new Coordinate(j-1,i-1);
+                    Coordinate c2 = new Coordinate(j-1,i);
+                    Coordinate c3 = new Coordinate(j-1,i+1);
+                    Coordinate c4 = new Coordinate(j,i-1);
+                    Coordinate c5 = new Coordinate(j,i+1);
+                    Coordinate c6 = new Coordinate(j+1,i-1);
+                    Coordinate c7 = new Coordinate(j+1,i);
+                    Coordinate c8 = new Coordinate(j+1,i+1);
+                    for (int c = 0; c <8;c++)
+                    {
+                        if (c1.isValid() && grid.getCellAt(c0).getState() == State.TOWER)
+                        {
+                            
+                             //TODO : finir
+                        }
+                    }
+                }
+            }
+        }
+        Random r = new Random();
+        return mesCoups.get(r.nextInt(mesCoups.size()));
     }
     
     
