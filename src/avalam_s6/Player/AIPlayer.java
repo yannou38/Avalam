b198600365a6b7ad045428e12cd02fdd32269403
TheDoctor <TheDoctor@TARDIS>
2016-05-12 08:04:02 +0200

    Added Player interface as well as AIPlayer and ControlledPlayer classes.

---
+++ src/avalam_s6/Player/AIPlayer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avalam_s6.Player;
+
+/**
+ *
+ * @author TheDoctor
+ */
+public class AIPlayer implements Player{
+
+    @Override
+    public boolean isAI() {
+        return true;
+    }
+    
+}
---
+++ src/avalam_s6/Player/ControlledPlayer.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avalam_s6.Player;
+
+/**
+ *
+ * @author TheDoctor
+ */
+public class ControlledPlayer implements Player{
+
+    @Override
+    public boolean isAI() {
+        return false;
+    }
+    
+}
