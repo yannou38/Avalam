74c8ad6f7192330cd953f9b8d8625e23b12ca52a
ducruyy <ducruyy@127.0.0.1>
2016-05-13 11:02:34 +0200

    ajoute énumeration pour la création des pages plus simplement

---
+++ src/avalam_s6/GUI/WindowState.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avalam_s6.GUI;
+
+/**
+ *
+ * @author ducruyy
+ */
+public enum WindowState {
+    HOME, //page d'accueil, avec accès aux autres fenetres
+    PLAYERSELECT, //page choix des joueurs, pour lancer la partie
+    RULES, //pages règles
+    TUTORIAL, //page tutoriel
+    SETTINGS, //page options
+    ABOUT, //page crédits
+    BOARD; //page du jeu ou se déroule la partie 
+    
+}
