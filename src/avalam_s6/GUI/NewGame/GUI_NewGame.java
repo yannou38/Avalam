f0629c5ce5ec38604b00f10ce500e69bbdfe01ac
ducruyy <ducruyy@127.0.0.1>
2016-05-17 17:28:20 +0200

    ajout GUI_NewGame vide

---
+++ src/avalam_s6/GUI/NewGame/GUI_NewGame.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avalam_s6.GUI.NewGame;
+
+import javax.swing.*;
+
+/**
+ *
+ * @author ducruyy
+ */
+public class GUI_NewGame extends JPanel {
+    
+}
