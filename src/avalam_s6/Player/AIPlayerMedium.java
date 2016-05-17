a497830163721f096e668112036995f3412c7f03
Seawolf <Seawolf@R2D2>
2016-05-17 10:32:17 +0200

    Reorganized AIPlayer, added AIPlayerMedium skeleton

---
+++ src/avalam_s6/Player/AIPlayerMedium.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avalam_s6.Player;
+
+import avalam_s6.Core.Move;
+import avalam_s6.Core.Owner;
+import java.awt.Color;
+
+/**
+ *
+ * @author Seawolf
+ */
+public class AIPlayerMedium extends AIPlayer{
+
+    public AIPlayerMedium(String name, Color color, Owner owner) {
+        super(name, color, owner);
+    }
+
+    @Override
+    public Move play() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
