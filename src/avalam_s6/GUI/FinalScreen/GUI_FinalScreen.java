3c5678271098545b6f1d4e1aad43cbb0a09b6b96
IQbrod <uta38@hotmail.fr>
2016-05-18 11:18:56 +0200

    Ajout bouton de retour et gestion des Font sur l'ecran final Reste a placer les boutons.

--- src/avalam_s6/GUI/FinalScreen/GUI_FinalScreen.java
+++ src/avalam_s6/GUI/FinalScreen/GUI_FinalScreen.java
@@ -5,12 +5,19 @@
  */
 package avalam_s6.GUI.FinalScreen;
 
+import avalam_s6.GUI.HomePage.HomePageListener;
+import java.awt.Font;
+import java.awt.FontFormatException;
 import java.awt.Graphics;
+import java.awt.GraphicsEnvironment;
 import java.awt.Image;
 import java.io.File;
+import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -21,9 +28,9 @@ import javax.swing.JPanel;
  */
 public class GUI_FinalScreen extends JPanel {
     public String theme;
-    private Image background, retI;
+    private Image background, homeI;
     private JLabel victoryText;
-    private JButton ret;
+    private JButton home;
     
     public GUI_FinalScreen() {
         this("Default");
@@ -31,18 +38,40 @@ public class GUI_FinalScreen extends JPanel {
     
     public GUI_FinalScreen(String theme) {
         this.theme = theme;
-        this.init();
+        this.victoryText = new JLabel();
+        this.initComponents();
     }
     
-    private void init() {
+    private void initComponents() {
         try {
+            /* Chargement de la police */
+            try {
+                Font localFont = Font.createFont(Font.TRUETYPE_FONT, new File("./ressources/Themes/" + this.theme + "/font/Gamaliel.otf"));
+                GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(localFont);
+                /* Application de la police */
+                this.victoryText.setFont(localFont.deriveFont(2*30f)); /* On peut appliquer un ratio a la police (ici 2) */
+                /* Aggrandissement des bordures */
+                this.victoryText.setBorder(BorderFactory.createEmptyBorder(0, 20, 20, 20));
+            } catch (IOException|FontFormatException ex) {
+                Logger.getLogger(GUI_FinalScreen.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            /* Chargement des images // du theme */
             this.background = ImageIO.read(new File("./ressources/Themes/" + this.theme + "/final/background.jpg"));
+            this.homeI = ImageIO.read(new File("./ressources/Themes/" + this.theme + "/final/home.png"));
         } catch (Exception ex) {
             Logger.getLogger(GUI_FinalScreen.class.getName()).log(Level.SEVERE, null, ex);
         }
         
-        this.victoryText = new JLabel();
+        
+        this.home = new JButton(new ImageIcon(this.homeI));
+        this.home.setBorder(BorderFactory.createEmptyBorder());
+        this.home.setContentAreaFilled(false);
+        this.home.setFocusPainted(false);
+        //home.addMouseListener(/*new HomePageListener("quit",theme)*/);
+        
+        //this.setLayout(null);
         this.add(this.victoryText);
+        this.add(this.home);
     }
     
     public void setWinner(String p) {
