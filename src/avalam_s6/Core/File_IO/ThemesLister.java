bd5e5392f648c9fbb2c0fc168c381694b1eacad0
IQbrod <uta38@hotmail.fr>
2016-05-20 12:45:00 +0200

    ThemesLister permet de lister tous les themes dans le repertoire ressources/Themes

---
+++ src/avalam_s6/Core/File_IO/ThemesLister.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package avalam_s6.Core.File_IO;
+
+import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ *
+ * @author sazeratj
+ */
+public class ThemesLister {
+    public String[] listThemes() {
+        Path p = Paths.get("./ressources/Themes");
+        if (Files.exists(p) && Files.isDirectory(p)) {
+            File f = new File("./ressources/Themes");
+            int x = f.list().length;
+            for (String s : f.list()) {
+                if (s.contains("."))
+                    x--;
+            }
+            String[] rStr = new String[x];
+            x = 0;
+            for (String s : f.list()) {
+                if (! s.contains(".")) {
+                    rStr[x] = s;
+                    x++;
+                }
+            }
+            return rStr;
+        }
+        return null;
+    }
+}
