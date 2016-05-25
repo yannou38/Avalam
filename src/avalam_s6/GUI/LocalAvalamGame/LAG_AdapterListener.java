733452a94b62e26a10d79e8ffb6bf5397e5ffc44
ducruyy <ducruyy@127.0.0.1>
2016-05-25 09:27:55 +0200

    petite modif 1280/720 => 1920/1080

--- src/avalam_s6/GUI/LocalAvalamGame/LAG_AdapterListener.java
+++ src/avalam_s6/GUI/LocalAvalamGame/LAG_AdapterListener.java
@@ -28,26 +28,26 @@ public class LAG_AdapterListener implements ComponentListener {
 
         Insets insets = page.getInsets();
         //1280*720 => taille de base
-        double ratioW = (double) page.getWidth() / (double) 1280;
-        double ratioH = (double) page.getHeight() / (double) 720;
+        double ratioW = (double) page.getWidth() / (double) 1920;
+        double ratioH = (double) page.getHeight() / (double) 1080;
         Dimension size = page.getGrille().getPreferredSize();
-        page.getGrille().setBounds((int) round((427 + insets.left) * ratioW), ((int) round((161 + insets.top) * ratioH)), size.width, size.height);
+        page.getGrille().setBounds((int) round((640 + insets.left) * ratioW), ((int) round((240 + insets.top) * ratioH)), size.width, size.height);
         page.getGrille().setSize((int) round(page.getGrille().getWidth() * ratioW), (int) round(page.getGrille().getHeight() * ratioH));
 
         size = page.getUndoB().getPreferredSize();
-        page.getUndoB().setBounds((int) round((400 + insets.left) * ratioW), ((int) round((620 + insets.top) * ratioH)), size.width, size.height);
+        page.getUndoB().setBounds((int) round((600 + insets.left) * ratioW), ((int) round((930 + insets.top) * ratioH)), size.width, size.height);
         page.getUndoB().setSize((int) round(page.getUndoB().getWidth() * ratioW), (int) round(page.getUndoB().getHeight() * ratioH));
 
         size = page.getRedoB().getPreferredSize();
-        page.getRedoB().setBounds((int) round((705 + insets.left) * ratioW), ((int) round((620 + insets.top) * ratioH)), size.width, size.height);
+        page.getRedoB().setBounds((int) round((1053 + insets.left) * ratioW), ((int) round((930 + insets.top) * ratioH)), size.width, size.height);
         page.getRedoB().setSize((int) round(page.getRedoB().getWidth() * ratioW), (int) round(page.getRedoB().getHeight() * ratioH));
 
         size = page.getSaveB().getPreferredSize();
-        page.getSaveB().setBounds((int) round((1101 + insets.left) * ratioW), ((int) round((620 + insets.top) * ratioH)), size.width, size.height);
+        page.getSaveB().setBounds((int) round((1650 + insets.left) * ratioW), ((int) round((930 + insets.top) * ratioH)), size.width, size.height);
         page.getSaveB().setSize((int) round(page.getSaveB().getWidth() * ratioW), (int) round(page.getSaveB().getHeight() * ratioH));
 
         size = page.getRetourB().getPreferredSize();
-        page.getRetourB().setBounds((int) round((3 + insets.left) * ratioW), ((int) round((620 + insets.top) * ratioH)), size.width, size.height);
+        page.getRetourB().setBounds((int) round((4 + insets.left) * ratioW), ((int) round((930 + insets.top) * ratioH)), size.width, size.height);
         page.getRetourB().setSize((int) round(page.getRetourB().getWidth() * ratioW), (int) round(page.getRetourB().getHeight() * ratioH));
     }
 
