f570c10db2821c448666712fdb899464dba0ca52
IQbrod <uta38@hotmail.fr>
2016-05-26 13:28:33 +0200

    Ecran Chargement termine

--- src/avalam_s6/GUI/Load/GUI_Load.java
+++ src/avalam_s6/GUI/Load/GUI_Load.java
@@ -126,7 +126,7 @@ public class GUI_Load extends JPanel implements Gui_INTERFACE {
                 if (!sil.getEmptyslot()) {
                     sil = new SaveInfoLister("slot_" + j);
                     this.slotlabels[i].setFont(this.slotlabels[i].getFont().deriveFont(1 * 30f));
-                    this.slotlabels[i].setText(sil.getDate() + " " + sil.getPlayer1() + " VS " + sil.getPlayer2() + " "+LanguageManager.getElement("Sur")+" " + sil.getGrid());
+                    this.slotlabels[i].setText(sil.getDate() + "    " + sil.getPlayer1() + " VS " + sil.getPlayer2() + "    "+LanguageManager.getElement("Sur")+" " + sil.getGrid());
                 }
 
             } catch (IOException ex) {
@@ -191,5 +191,8 @@ public class GUI_Load extends JPanel implements Gui_INTERFACE {
     public void callResize() {
         this.callResize = true;
     }
-
+    
+    public void resetSlotSelection() {
+        this.slotnumber = 0;
+    }
 }
