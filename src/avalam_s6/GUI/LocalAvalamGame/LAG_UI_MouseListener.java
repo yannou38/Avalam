6db1dbfcd63807356bb14bf38fc4d1219335ba43
ducruyy <ducruyy@127.0.0.1>
2016-06-02 12:44:40 +0200

    Javadoc du package GUI_LAG

--- src/avalam_s6/GUI/LocalAvalamGame/LAG_UI_MouseListener.java
+++ src/avalam_s6/GUI/LocalAvalamGame/LAG_UI_MouseListener.java
@@ -19,13 +19,13 @@ import static java.lang.Math.round;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.imageio.ImageIO;
-import static javax.imageio.ImageIO.read;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 
 /**
+ * Listener on the board panel.
  *
- * @author ducruyy
+ * @author Team 7
  */
 public class LAG_UI_MouseListener implements MouseListener {
 
@@ -34,18 +34,24 @@ public class LAG_UI_MouseListener implements MouseListener {
     private Image iconbase;
     private GUI_LAG page;
 
+    /**
+     * Constructor
+     *
+     * @param buttonname the button name, also used for image loading
+     * @param page the board panel
+     */
     public LAG_UI_MouseListener(String buttonname, GUI_LAG page) {
         this.name = buttonname;
         this.page = page;
-        if(this.name == "play"){
+        if ("play".equals(this.name)) {
             try {
                 this.icon = ImageIO.read(new File("./ressources/Themes/" + SetupManager.getElement("Theme") + "/" + SetupManager.getElement("Langue") + "/board/pause_h.png"));
                 this.iconbase = ImageIO.read(new File("./ressources/Themes/" + SetupManager.getElement("Theme") + "/" + SetupManager.getElement("Langue") + "/board/pause.png"));
             } catch (Exception ex) {
                 Logger.getLogger(GUI_HomePage.class.getName()).log(Level.SEVERE, null, ex);
             }
-        } else if (this.name == "mute") {
-            String muteS = SetupManager.getElement("Son").equals("Non")?"unmute":"mute";
+        } else if ("mute".equals(this.name)) {
+            String muteS = SetupManager.getElement("Son").equals("Non") ? "unmute" : "mute";
             try {
                 this.icon = ImageIO.read(new File("./ressources/Themes/" + SetupManager.getElement("Theme") + "/" + SetupManager.getElement("Langue") + "/board/" + muteS + "_h.png"));
                 this.iconbase = ImageIO.read(new File("./ressources/Themes/" + SetupManager.getElement("Theme") + "/" + SetupManager.getElement("Langue") + "/board/" + muteS + ".png"));
@@ -62,6 +68,11 @@ public class LAG_UI_MouseListener implements MouseListener {
         }
     }
 
+    /**
+     * Handler for the mouse clicked event.
+     *
+     * @param e the mouse event
+     */
     @Override
     public void mouseClicked(MouseEvent e) {
         JButton source = (JButton) e.getSource();
@@ -112,8 +123,8 @@ public class LAG_UI_MouseListener implements MouseListener {
                 break;
             case "mute":
                 SoundEngine.toggleMute();
-                try{
-                    if (SoundEngine.isMuted()) {                
+                try {
+                    if (SoundEngine.isMuted()) {
                         this.icon = ImageIO.read(new File("./ressources/Themes/" + SetupManager.getElement("Theme") + "/" + SetupManager.getElement("Langue") + "/board/unmute_h.png"));
                         this.iconbase = ImageIO.read(new File("./ressources/Themes/" + SetupManager.getElement("Theme") + "/" + SetupManager.getElement("Langue") + "/board/unmute.png"));
                     } else {
@@ -131,8 +142,8 @@ public class LAG_UI_MouseListener implements MouseListener {
                 break;
             case "play":
                 game.togglePause();
-                try{
-                    if (((Local_Avalam_Game) this.page.getGame()).isPaused()) {                
+                try {
+                    if (((Local_Avalam_Game) this.page.getGame()).isPaused()) {
                         this.icon = ImageIO.read(new File("./ressources/Themes/" + SetupManager.getElement("Theme") + "/" + SetupManager.getElement("Langue") + "/board/play_h.png"));
                         this.iconbase = ImageIO.read(new File("./ressources/Themes/" + SetupManager.getElement("Theme") + "/" + SetupManager.getElement("Langue") + "/board/play.png"));
                     } else {
@@ -142,44 +153,64 @@ public class LAG_UI_MouseListener implements MouseListener {
                 } catch (IOException ex) {
                     Logger.getLogger(LAG_UI_MouseListener.class.getName()).log(Level.SEVERE, null, ex);
                 }
-                this.page.setCallPause(true);                               
+                this.page.setCallPause(true);
                 this.page.setPlay(this.icon);
-                
+
                 this.page.repaint();
-                
+
                 break;
         }
 
     }
 
+    /**
+     * Do nothing.
+     *
+     * @param e the mouse event
+     */
     @Override
     public void mousePressed(MouseEvent e) {
     }
 
+    /**
+     * Do nothing.
+     *
+     * @param e the mouse event
+     */
     @Override
     public void mouseReleased(MouseEvent e) {
     }
 
+    /**
+     * Handler for hovering on the button.
+     *
+     * @param e the mouse event
+     */
     @Override
     public void mouseEntered(MouseEvent e) {
         //replace the icon with another
-        
-        if (this.name.equals("play")) {            
+
+        if (this.name.equals("play")) {
             this.page.setPlay(this.icon);
         }
-        
-        if (this.name.equals("mute")){            
+
+        if (this.name.equals("mute")) {
             this.page.setMute(this.icon);
         }
-        
+
         JButton source = (JButton) e.getSource();
         GUI_LAG lag = ((GUI_LAG) source.getParent());
         double ratioW = (double) lag.getWidth() / (double) 1920;
         double ratioH = (double) lag.getHeight() / (double) 1080;
         Image newimg = this.icon.getScaledInstance(((int) round(icon.getWidth(null) * ratioW)), ((int) round(icon.getHeight(null) * ratioH)), java.awt.Image.SCALE_SMOOTH);
-        ((JButton) e.getSource()).setIcon(new ImageIcon(newimg));        
+        ((JButton) e.getSource()).setIcon(new ImageIcon(newimg));
     }
 
+    /**
+     * Handler for hovering on the button.
+     *
+     * @param e the mouse event
+     */
     @Override
     public void mouseExited(MouseEvent e) {
         //replace the icon with another
@@ -193,7 +224,7 @@ public class LAG_UI_MouseListener implements MouseListener {
         if (this.name.equals("play")) {
             this.page.setPlay(this.iconbase);
         }
-        
+
         if (this.name.equals("mute")) {
             this.page.setMute(this.iconbase);
         }
